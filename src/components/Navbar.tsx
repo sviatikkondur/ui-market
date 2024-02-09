6a1137891209113df17acacab7148827ab3a5036
Sviatoslav Kondur <107207978+sviatikkondur@users.noreply.github.com>
2024-02-09 17:41:48 +0100

    finish header for desktop and create a cart drawer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { Icons } from './Icons';
 import NavItems from './NavItems';
+import { buttonVariants } from './ui/button';
+import Cart from './Cart';
 
 const Navbar = () => {
+  const user = null;
+
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
       <header className='relative bg-white'>
@@ -24,7 +28,52 @@ const Navbar = () => {
 
               <div className='ml-auto flex items-center'>
                 <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
-                  {}
+                  {!user && (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Sign In
+                    </Link>
+                  )}
+
+                  {!user && (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {user ? (
+                    <h1>Log out</h1>
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({ variant: 'ghost' })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+
+                  {user && (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {!user && (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
                 </div>
               </div>
             </div>
